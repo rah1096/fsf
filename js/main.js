7f5457ee1e077875bb0a74710849c8247704d796
Tim <chent1101@gmail.com>
2015-12-27 23:00:12 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function() {
+    //Sub menu dropdowns
+    $("#events").hover(function() {
+        $("#events ul").show();
+    },
+    function() {
+        $("#events ul").hide();
+    });
+
+    $("#recreational_healing").hover(function() {
+        $("#recreational_healing ul").show();
+    },
+    function() {
+        $("#recreational_healing ul").hide();
+    });
+
+    $("#coaching").hover(function() {
+        $("#coaching ul").show();
+    },
+    function() {
+        $("#coaching ul").hide();
+    });
+
+});
